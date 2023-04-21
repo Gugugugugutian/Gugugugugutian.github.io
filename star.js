04c5c307c04dd2bfc3a1461cece10abea9366934
GUTIAN <529131196@qq.COM>
2023-04-21 21:57:52 +0800

    New checkin function

--- star.js
+++ star.js
@@ -1,17 +1,29 @@
+function dynamic_color(){
+  // console.log("The setInterval function is running!");
+  var date = new Date();
+  var seconds = date.getSeconds();
+  var color = Math.floor(seconds / 60 * 255).toString(16).padStart(2, "0");
+  var textColor = "#" + color + color + color;
+  console.log(textColor);
+  var state=document.getElementsById("lar");
+  console.log("state:"+state.color);
+  state.color = textColor + " !important";
+}
+
 // get the width and height of the page
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-// create 50 white dots
-for (var i = 0; i < 50; i++) {
-  // create a new star element
-  var star = document.createElement("div");
-  star.className = "star";
+// // create 50 white dots
+// for (var i = 0; i < 50; i++) {
+//   // create a new star element
+//   var star = document.createElement("div");
+//   star.className = "star";
 
-  // set the top and left positions to random values within the page dimensions
-  star.style.top = Math.random() * height + "px";
-  star.style.left = Math.random() * width + "px";
+//   // set the top and left positions to random values within the page dimensions
+//   star.style.top = Math.random() * height + "px";
+//   star.style.left = Math.random() * width + "px";
 
-  // add the star element to the #stars container
-  document.getElementById("stars").appendChild(star);
-}
+//   // add the star element to the #stars container
+//   document.getElementById("stars").appendChild(star);
+// }
