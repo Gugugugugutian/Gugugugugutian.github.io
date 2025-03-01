49ddbe63ade5b9945c16d4c9fa54c00dbf648fde
GUTIAN <529131196@qq.COM>
2025-03-01 23:36:10 +0800

    new version

---
+++ assets/ChatView-13fu-DRK.js
@@ -0,0 +1,1 @@
+import{r as e,c as t,o as a}from"./index-KBU5JhGE.js";const s={class:"chat-main"},n={__name:"ChatView",setup(c){return e([{role:"system"}]),(o,r)=>(a(),t("div",s," 敬请期待 "))}};export{n as default};
--- assets/ProfileView-DIEEgaQO.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-CQY5yDxU.js";const s={},t={class:"profile-main"};function r(_,a){return o(),c("div",t," 敬请期待 ")}const i=e(s,[["render",r],["__scopeId","data-v-63cee120"]]);export{i as default};
